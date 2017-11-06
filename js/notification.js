6d1b794b33bf9653c348f2910380791650fe6a7c
Yomar Miranda <mirandayomar@gmail.com>
2017-11-05 23:45:01 -0500

    De esta manera logramos hacer nuestro contador de notificaciones dinamico, creamos una variable llamada 'count' la cual obtiene el número de notificaciones que se encuentren dentro del atributo 'data-count' en caso de ser vacio o indefinido se inicializará en 0 para luego ir aumentando de 1 en 1

--- js/notification.js
+++ js/notification.js
@@ -2,5 +2,7 @@ $button = document.getElementById('button');
 $campana = document.getElementById('campana');
 
 $button.addEventListener('click', (event) => {
+	let count = Number($campana.getAttribute('data-count')) || 0;
+	$campana.setAttribute('data-count', count + 1);
 	$campana.classList.add('show-count');
 })
